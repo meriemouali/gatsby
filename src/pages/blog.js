3979f1b6ba1289f4e5418e27d3e4c5e973cffb96
MariemOuali <mariem.ouali@target-energysolutions.com>
2020-01-22 11:05:59 +0100

    :art: add pages with gatsby

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPage=()=>{
+    return(
+        <div>
+        <h1>Blog</h1>
+        <p>Posts will show up here </p>
+        </div>
+    )
+}
+export default BlogPage
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactPage=()=>{
+    return(
+        <div>
+        <h1>Contacts</h1>
+        <p>the Best way to reach me is via My Twitter @OUALI_Meriem</p>
+        </div>
+    )
+}
+export default ContactPage
