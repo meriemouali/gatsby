b493399cd34e4b287f3450b1eb9c32bff7eb7fef
MariemOuali <mariem.ouali@target-energysolutions.com>
2020-01-22 15:23:00 +0100

    :star: add graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title:'My Personal Site',
+    author:'Meriem Ouali',
+  },
   plugins:[
     'gatsby-plugin-sass'
   ]
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import {Link} from 'gatsby'
+import {Link,graphql,useStaticQuery} from 'gatsby'
 
 
 import headerStyles  from './header.module.scss'
 
 const Header=()=>{
+    const data=useStaticQuery(graphql`
+    query {
+        site
+        {
+            siteMetadata
+            {
+                title
+                author
+            }
+        }
+    }`)
     return(
         <header className={headerStyles.header}>
-            <h1><Link className={headerStyles.title} to='/'>Meriem Ouali</Link></h1>
+            <h1><Link className={headerStyles.title} to='/'>{data.site.siteMetadata.title}</Link></h1>
             <nav>
                 <ul className={headerStyles.navList}>
                     <li >
